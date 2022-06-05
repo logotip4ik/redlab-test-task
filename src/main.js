d471dffdfc1e3c18e7f2f8eb38d82b32d433a7c0
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-05 19:18:22 +0300

    update: registered scroll trigger with gsap

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import './assets/fonts/fonts.css';
 import './assets/styles/global.css';
 
 import { createApp } from 'vue';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import App from './App.vue';
+
+gsap.registerPlugin(ScrollTrigger);
 
 createApp(App).mount('#app');
