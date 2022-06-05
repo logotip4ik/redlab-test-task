082e81458a92e41049344782826e896896cec47b
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-05 18:46:03 +0300

    update: added config for unplugin components

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,21 @@ export default defineConfig({
     AutoImport({
       dirs: ['./src/hooks', './src/composables'],
       imports: ['vue'],
+    }),
+    Components({
+      dirs: ['src/components'],
+      extensions: ['vue'],
+      deep: true,
+      dts: false,
+      directoryAsNamespace: true,
+      globalNamespaces: [],
+      directives: true,
+      include: [/\.vue$/, /\.vue\?vue/],
+      exclude: [
+        /[\\/]node_modules[\\/]/,
+        /[\\/]\.git[\\/]/,
+        /[\\/]\.nuxt[\\/]/,
+      ],
     }),
   ],
 });
