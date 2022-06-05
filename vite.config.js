5c2bd9f2314ce2efe0c68628319e9cf00035504d
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-05 18:13:10 +0300

    fix: wrong import url

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
-import AutoImport from 'unplugin-auto-import';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
