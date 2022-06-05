20071ed844bc4e5df9c680c8956b69c5f6d234c9
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-05 18:26:02 +0300

    update: added proper config for auto import plugin

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Vue(), AutoImport({ dts: true })],
+  plugins: [
+    Vue(),
+    AutoImport({
+      dirs: ['./src/hooks', './src/composables'],
+      imports: ['vue'],
+    }),
+  ],
 });
