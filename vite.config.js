d23b35ae429b48e7f05fe52cbed444a1bd0a3bc6
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-05 20:27:32 +0300

    update: added svg loader plugin

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
+import SvgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -26,5 +27,6 @@ export default defineConfig({
         /[\\/]\.nuxt[\\/]/,
       ],
     }),
+    SvgLoader(),
   ],
 });
