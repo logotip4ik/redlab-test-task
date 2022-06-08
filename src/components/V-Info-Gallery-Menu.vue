760600734b6f5581da66bbf5ece941d6647039dc
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-08 17:20:43 +0300

    update: chanaged a bit styles and added probably support for mobile sized screens

---
+++ src/components/V-Info-Gallery-Menu.vue
@@ -0,0 +1,13 @@
+<script setup>
+import useEmitter from '../hooks/use-emitter';
+
+const menuSections = ['Кухня', 'Комнаты', 'Ванная', 'Прихожая'];
+
+const emitter = useEmitter();
+
+console.log(emitter);
+</script>
+
+<template>
+  <div class="info-gallery-menu"></div>
+</template>
