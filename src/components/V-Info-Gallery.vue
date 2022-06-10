85177153ed2e67ca6e85273d028f5f7fe1bfcbae
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-06-10 23:14:58 +0300

    update: used newly created tooltip and changed a bit styles for phones

--- src/components/V-Info-Gallery.vue
+++ src/components/V-Info-Gallery.vue
@@ -4,6 +4,7 @@ import InfoImage2 from '../assets/images/info-gallery-image-2.png';
 import useGsap from '../hooks/use-gsap';
 import useEmitter from '../hooks/use-emitter';
 import useCurrentSection from '../hooks/use-current-section';
+import useElementSize from '../hooks/use-element-size';
 
 const emitter = useEmitter();
 const { gsap, Flip, ScrollTrigger } = useGsap();
@@ -12,6 +13,7 @@ const infoGalleryHeaderRef = ref(null);
 const infoGallerySlidesRef = ref(null);
 
 const currentSection = useCurrentSection();
+const infoGallerySlidesSize = useElementSize(infoGallerySlidesRef);
 const slides = [
   { label: 'Кухня', image: InfoImage1 },
   { label: 'Комнаты', image: InfoImage2 },
@@ -20,20 +22,25 @@ const slides = [
 ];
 
 function imageEnterAnim(imgEl, done) {
-  gsap.fromTo(
-    imgEl,
-    { clipPath: 'inset(0 0 0 100%)', scale: 1.25 },
-    {
-      clipPath: 'inset(0 0 0 0%)',
-      scale: 1,
-      ease: 'expo.out',
-      duration: 0.85,
-      onComplete: done,
-    }
-  );
+  gsap
+    .fromTo(
+      imgEl,
+      { clipPath: 'inset(0 0 0 100%)', scale: 1.25 },
+      {
+        clipPath: 'inset(0 0 0 0%)',
+        scale: 1,
+        ease: 'expo.out',
+        duration: 0.85,
+        onComplete: done,
+      }
+    )
+    .then(() =>
+      gsap.to('.tooltip', { autoAlpha: 1, duration: 0.25, stagger: 0.065 })
+    );
 }
 
 function imageLeaveAnim(imgEl, done) {
+  gsap.to('.tooltip', { autoAlpha: 0, duration: 0.1 });
   gsap.set(imgEl, {
     position: 'absolute',
     top: 0,
@@ -53,6 +60,8 @@ function pinVideo(func) {
 }
 
 onMounted(() => {
+  gsap.set('.tooltip', { autoAlpha: 0 });
+
   const trigger = ScrollTrigger.create({
     trigger: infoGalleryHeaderRef.value,
     start: 'bottom 17%',
@@ -60,8 +69,12 @@ onMounted(() => {
     onEnter() {
       emitter.emit('gallery:fixed');
       pinVideo('add');
+
+      gsap.to('.tooltip', { autoAlpha: 1, stagger: 0.065 });
     },
     onLeaveBack() {
+      gsap.to('.tooltip', { autoAlpha: 0 });
+
       emitter.emit('gallery:static');
       pinVideo('remove');
     },
@@ -89,8 +102,17 @@ onMounted(() => {
           :key="currentSection"
           class="info-gallery__slides__image"
           decode="async"
+          loading="lazy"
         />
       </Transition>
+
+      <Utils-VTooltip
+        text="Чистим фасад витяжки"
+        :x="35"
+        :y="39"
+        :container-width="infoGallerySlidesSize.width"
+        :container-height="infoGallerySlidesSize.height"
+      />
     </div>
 
     <div class="info-gallery__spacer"></div>
@@ -130,6 +152,7 @@ onMounted(() => {
 
   &__slides {
     position: relative;
+    z-index: 1;
 
     width: 100%;
     height: 100%;
@@ -167,19 +190,23 @@ onMounted(() => {
       @media screen and (max-width: 1100px) {
         top: unset;
         left: 0;
+        right: 0;
         bottom: 25%;
 
-        width: 100%;
-        height: 55vh;
+        width: 100vw;
+        height: 52vh;
 
         max-height: 75%;
-        min-height: 55vh;
       }
     }
   }
 
   &__spacer {
     min-height: 100vh;
+
+    @media screen and (max-width: 1100px) {
+      min-height: 75vh;
+    }
   }
 }
 </style>
